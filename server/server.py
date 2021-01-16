df8096fd64196ec312892898f386eac27e4d02b2
Matt Zhang <mcmattzhang@gmail.com>
2021-01-16 00:55:15 -0500

    D:/Program Files/Git/reddtCSV/username now downloads csv

--- server/server.py
+++ server/server.py
@@ -1,7 +1,6 @@
-from flask import Flask
-from flask import jsonify
+from flask import Flask,make_response,jsonify
 import requests 
-import json
+import json, csv, io
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
@@ -15,9 +14,38 @@ def getReddit(username):
     for post in redditRes.json()["data"]:
         out[post["permalink"]]=post["body"]
     return jsonify(out)
+    
+def getRedditCSV(username):
+    redditRes = requests.get('https://api.pushshift.io/reddit/search/comment/?author='+username+'&size=100')
+    out = "subreddit, created_time, content"
+    for post in redditRes.json()["data"]:
+        out += post["subreddit"] + ","
+        out += str(post["created_utc"]) + ","
+        out += post["body"] + "\n"
+        
+    return out
+
+# def getRedditCSVList(username):
+#     redditRes = requests.get('https://api.pushshift.io/reddit/search/comment/?author='+username+'&size=100')
+
+#     dest = io.StringIO()
+#     writer = csv.writer(dest)
+
+#     writer.writerow(["subreddit", "created_time", "content"])
+#     for post in redditRes.json()["data"]:
+#         writer.writerow([post["subreddit"],str(post["created_utc"]),post["body"]])
+        
+#     return dest
 
 @app.route('/reddit/<username>')
 def profile(username):
     return getReddit(username)
+
+@app.route('/redditCSV/<username>')
+def getReddit(username):
+    response = make_response(getRedditCSV(username))
+    response.headers['Content-Disposition'] = 'attachment; filename='+username+'.csv' 
+    response.mimetype='text/csv'
+    return response
 
 app.run()
