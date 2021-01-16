a086d5383d1b1764d654deb21e59b861fcd35415
Matt Zhang <mcmattzhang@gmail.com>
2021-01-16 00:14:55 -0500

    implemented getReddit

--- server/server.py
+++ server/server.py
@@ -1,10 +1,23 @@
 from flask import Flask
-
+from flask import jsonify
+import requests 
+import json
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
 @app.route('/hello', methods=['GET'])
 def hello_world():
     return 'Hello World!'
+
+def getReddit(username):
+    redditRes = requests.get('https://api.pushshift.io/reddit/search/comment/?author='+username+'&size=100')
+    out = {}
+    for post in redditRes.json()["data"]:
+        out[post["permalink"]]=post["body"]
+    return jsonify(out)
+
+@app.route('/reddit/<username>')
+def profile(username):
+    return getReddit(username)
 
 app.run()
