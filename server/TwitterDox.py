f42bc30968a12ba8e0743b8e2c2985bd56e432a7
Aurik-Datta <aurik.duky@gmail.com>
2021-01-16 15:33:52 -0500

    cleaned csv

--- server/TwitterDox.py
+++ server/TwitterDox.py
@@ -18,6 +18,8 @@ def getTimeline(userid):
     TimelineRes = requests.get('https://api.twitter.com/2/users/'+userid+'/tweets?max_results=100&tweet.fields=created_at', headers = headers).json()
     data = list()
     for i in TimelineRes['data']:
+        i['text'] = i['text'].replace(",", " ")
+        i['text'] = i['text'].replace("\n", "")
         data.append({'text':i['text'], 'created_at':i['created_at']})
     with open('tweets.csv','w',encoding='utf8', newline='') as output_file:
         fc=csv.DictWriter(output_file,fieldnames=data[0].keys(),)
