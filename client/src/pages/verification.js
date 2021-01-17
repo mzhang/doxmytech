da461582d76124f444fe4f68d57642037ad4c12f
Matt Zhang <mcmattzhang@gmail.com>
2021-01-16 23:37:40 -0500

    compenented cards w/ new logic

--- client/src/pages/verification.js
+++ client/src/pages/verification.js
@@ -1,10 +1,9 @@
 import React, { useState } from "react";
 import "./verification.css"
-
-import Card from "react-bootstrap/Card"
-import FacebookLogin from 'react-facebook-login';
-import Button from "react-bootstrap/Button"
 import axios from "axios";
+
+import {FacebookCard, TwitterCard, SuccessCard} from "../components/card.js"
+
 
 //import NodeFetch from "node-fetch";
 
@@ -36,50 +35,31 @@ export default function Verification(props) {
             url: 'http://localhost:5000/twitterBio/' + props.location.state.twitter
         })
 
-        var bio = res.data;
+        let bio = res.data;
         console.log(bio)
         if (bio.toLowerCase() === "doxmytech") {
             setTwitterData("twitter bio is correct");
         }
     }
-    
+
+
     // You can access the user's twitter/facebook/reddit using props.location.state
     // For example, props.location.state.reddit gives the user's supposed reddit username
-    
+
+
+    function summonCards() {
+        let outArray = []
+        if (props?.location?.state?.facebook) outArray.push((fbData) ? <SuccessCard title={"Facebook"} /> : <FacebookCard responseFacebook={responseFacebook} fbData={fbData}/>)
+        if (props?.location?.state?.twitter) outArray.push((twitterData) ? <SuccessCard title={"Twitter"} /> : <TwitterCard responseTwitter={responseTwitter} twitterData={twitterData}/>)
+        return outArray
+    }
+
     return (
         <div>
             <div style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
                 <h1>Hold on...</h1>
                 <h2>We need your permission before continuing.</h2>
-
-                <Card className="text-center top-margins">
-                    <Card.Header>Facebook</Card.Header>
-                    <Card.Body style={{ display: (fbData) ? 'none' : 'block'}}>
-
-                        <FacebookLogin
-                            appId="440751897337373"
-                            fields="id"
-                            scope="public_profile,email"
-                            autoLoad={true}
-                            callback={responseFacebook} />
-                        <Card.Text>
-                            We only access publicly available info and you can remove access later.
-                        </Card.Text>
-                    </Card.Body>
-                </Card>
-
-                <Card className="text-center top-margins">
-                    <Card.Header>Twitter</Card.Header>
-                    <Card.Body style={{ display: (twitterData) ? 'none' : 'block'}}>
-
-                        <Card.Text>
-                            Add the line <code>DoxMyTech</code> to your Twitter bio to give us permission!
-                        </Card.Text>
-
-                        <Button variant="primary" size="lg" onClick={responseTwitter} active>Verify my Twitter bio</Button>
-
-                    </Card.Body>
-                </Card>
+                {summonCards()}
 
             </div>
         </div>
