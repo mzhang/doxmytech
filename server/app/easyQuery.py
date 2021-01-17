ae5cf8e426715184bfaa7156a3ca4eed219cea4f
WolfVD <57068085+WolfVD@users.noreply.github.com>
2021-01-16 22:13:51 -0500

    list of strings

--- server/app/easyQuery.py
+++ server/app/easyQuery.py
@@ -1,8 +1,11 @@
-import requests, json 
+import requests, json, os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 def getQuery(query):
     tableToQuery = 'posts'
-    key = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhYmFzZUlkIjoibnBpb3Bna01DYVNQYU1qRndvOFluNSIsImFjY2Vzc1Blcm0iOiJmdWxsIiwidG9rZW5JZCI6ImJLS2NScUVOd0NsRmFpNHFyaEt3RUVvQ0duQTFMclJ2ODhBY3lBODJpSFExZHdDZ0J5MW93bjF4ZW1Lbm0zVnciLCJpYXQiOjE2MTA4NDQxMzMsImV4cCI6MTYxMTAxNjkzMywiaXNzIjoiZHJvcGJhc2UuaW8iLCJzdWIiOiJSZTVmOHhqekdrMlFrZUY2VnBKYThRIn0.NcbCcUk3VxgGfxgtktrHY2Ti5mdiS5Nolryu_Bs9RpQ'
-    r = requests.get('https://query.dropbase.io/npiopgkMCaSPaMjFwo8Yn5' + '/' + tableToQuery + query, headers = {"Authorization": key})
+    r = requests.get('https://query.dropbase.io/npiopgkMCaSPaMjFwo8Yn5' + '/' + tableToQuery + query, headers = {"Authorization": "Bearer " + os.getenv("DROPBASE_REST_SECRET")})
 
     return json.loads(r.text)
