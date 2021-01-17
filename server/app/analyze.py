1bdf4e31bdf5ee422afd6f8c5bf055ec1accdb36
WolfVD <57068085+WolfVD@users.noreply.github.com>
2021-01-16 21:33:20 -0500

    fixed env

--- server/app/analyze.py
+++ server/app/analyze.py
@@ -3,3 +3,10 @@ from flask import Flask
 from flask import make_response,jsonify,send_file,request
 import requests, json
 
+from facebook import facebookData
+
+@app.route("/analyzeLinks/<facebook>&<reddit>&<twitter>")
+def AnalyzeLinks(facebookID, facebookAccess, reddit, twitter): #facebookID = id, facebookAccess = access, reddit = username, twitter = username
+    facebookJSON = facebookData(facebookID, facebookAccess)
+
+    fullName = facebookJSON.
